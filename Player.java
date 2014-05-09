4b1bf38c309336bf071b52082b2a76eb37f702b3
Finbel <p.oregan.87@gmail.com>
2014-05-09 19:07:16 +0200

    Updated Player

--- Player.java
+++ Player.java
@@ -1,4 +1,15 @@
 
-public class Player {
+public class Player extends Character{
 
+	/**
+	 * Creates a player with name "name", health "health" and (x,y)-coordinates x and y.
+	 * 
+	 * @param name
+	 * @param health
+	 * @param x
+	 * @param y
+	 */
+	public Player(String name, int health, int x, int y){
+		super(name, health, x, y);
+	}
 }
